ee209019e49e85ab072210cd670af3e413f7cbf4
Maxim Kulikov <m.j.kulikov@gmail.com>
2021-12-17 12:37:04 +0300

    update LevelSettings and add UI skillTree

--- Assets/Scripts/Gameplay/LevelSystem/LevelSettings.cs
+++ Assets/Scripts/Gameplay/LevelSystem/LevelSettings.cs
@@ -1,29 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 [CreateAssetMenu(fileName = "LevelSettingsData",
     menuName = "ScriptableObject/LevelSettings/LevelSettingsData")]
 public class LevelSettings : ScriptableObject
 {
+    
     [SerializeField]
     private int[] _experienceAmountForLevel;
+
+    [SerializeField]
+    private List<LevelSetting> _levelsSettings;
+
+   
 
     public int ExperienceForNextLevel(int level)
     {
         if (level < GetMaxLevel())
-            return _experienceAmountForLevel[level - 1];
+            return _levelsSettings.Find(h => h.level == level).experienceForNextLevel;
         else
-            return _experienceAmountForLevel[_experienceAmountForLevel.Length - 1];
+            return _levelsSettings.Find(h => h.level == GetMaxLevel()).experienceForNextLevel;
     }
 
     public int GetMaxLevel()
     {
-        return _experienceAmountForLevel.Length;
+        return _levelsSettings.Count;
     }
 
     public bool IsMaxLevel(int level)
     {
-        return _experienceAmountForLevel.Length <= level;
+        return _levelsSettings.Count <= level;
+    }
+
+    [Serializable]
+    private struct LevelSetting
+    {
+        [SerializeField]
+        public int level;
+        [SerializeField]
+        public int experienceForNextLevel;
+        [SerializeField]
+        public int skillPoints;
     }
 }
