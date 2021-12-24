aee1659a0f33bc008e1257bbd772156b06c3184a
Maxim Kulikov <m.j.kulikov@gmail.com>
2021-12-24 17:41:36 +0300

    debug ReloadSysten

--- Assets/Scripts/Gameplay/Weapon/Reload/RocketVisibilityController.cs
+++ Assets/Scripts/Gameplay/Weapon/Reload/RocketVisibilityController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class RocketVisibilityController : MonoBehaviour
 {
@@ -12,9 +13,27 @@ public class RocketVisibilityController : MonoBehaviour
     [SerializeField]
     private WeaponReloadController _weaponReloadController;
 
+    private IInventoryItemManager _inventoryItemManager;
+
+    [Inject]
+    public void Construct(IInventoryItemManager inventoryItemManager)
+    {
+        _inventoryItemManager = inventoryItemManager;
+    }
+
+
     private void Awake()
     {
         _weaponReloadController.OnBulletsRunOut += OnBulletsRunOut;
+        _inventoryItemManager.OnItemAdded += OnItemAdded;
+    }
+
+    private void OnItemAdded(Item item)
+    {
+        if(false)
+        {
+            _rocketGO.SetActive(true);//на будещее, если будет норм инвентарь
+        }
     }
 
     private void OnBulletsRunOut(WeaponStateID obj)
