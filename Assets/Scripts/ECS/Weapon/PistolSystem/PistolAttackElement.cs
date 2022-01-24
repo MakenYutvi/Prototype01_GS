3be9c4745a5c2ad31a4bb50f787a6f34a9cf47a5
Maxim Kulikov <m.j.kulikov@gmail.com>
2022-01-24 18:10:01 +0300

    debug ecs

--- Assets/Scripts/ECS/Weapon/PistolSystem/PistolAttackElement.cs
+++ Assets/Scripts/ECS/Weapon/PistolSystem/PistolAttackElement.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 namespace ECS
 {
     public class PistolAttackElement : WeaponAttackElementMono
     {
+        [SerializeField]
+        private int _damage = 5;//задел на будущее
+        [SerializeField]
+        private GameObject _firePoint;
+
+
+        private IBulletManager _BulletManager;
+        public event ActionWeapon<Collider> OnCollideEvent;
+
+        [Inject]
+        public void Construct(IBulletManager bulletManager)
+        {
+            _BulletManager = bulletManager;
+        }
         public override void Attack()
         {
             Debug.Log("Pistol attack");
+            _BulletManager.LaunchBullet(this.transform.position + _firePoint.transform.forward, this.transform.rotation, _firePoint.transform.forward, null);
         }
 
         public override bool CanAttack()
